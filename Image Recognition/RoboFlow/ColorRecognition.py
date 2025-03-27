389e8439116ea0dd9bf7dbeb08fe9faa5f721bab
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2025-03-27 15:23:53 +0100

    Draw vectors bewteen green and blue objects

--- Image Recognition/RoboFlow/ColorRecognition.py
+++ Image Recognition/RoboFlow/ColorRecognition.py
@@ -29,46 +29,19 @@ def drawAxis(img, p_, q_, colour, scale):
     p[1] = q[1] + 9 * sin(angle - pi / 4)
     cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv2.LINE_AA)
 
-def getOrientation(pts, img):
-    sz = len(pts)
-    data_pts = np.empty((sz, 2), dtype=np.float64)
-    for i in range(data_pts.shape[0]):
-        data_pts[i, 0] = pts[i, 0, 0]
-        data_pts[i, 1] = pts[i, 0, 1]
-
-    # Perform PCA analysis
-    mean = np.empty(0)
-    mean, eigenvectors, eigenvalues = cv2.PCACompute2(data_pts, mean)
-
-    # Store the center of the object
-    cntr = (int(mean[0, 0]), int(mean[0, 1]))
-
-    cv2.circle(img, cntr, 3, (255, 0, 255), 2)
-    p1 = (cntr[0] + 0.02 * eigenvectors[0, 0] * eigenvalues[0, 0],
-        cntr[1] + 0.02 * eigenvectors[0, 1] * eigenvalues[0, 0])
-    p2 = (cntr[0] - 0.02 * eigenvectors[1, 0] * eigenvalues[1, 0],
-        cntr[1] - 0.02 * eigenvectors[1, 1] * eigenvalues[1, 0])
-    drawAxis(img, cntr, p1, (0, 255, 0), 1)
-    drawAxis(img, cntr, p2, (255, 255, 0), 5)
-
-    angle = atan2(eigenvectors[0, 1], eigenvectors[0, 0])  # orientation in radians
-
-    return angle
-
 
 while True:
     ret, imageFrame = cam.read()
-    img = imageFrame.copy()
 
-    middle_blue = 0
-    middle_green = 0
+    middle_blue = []
+    middle_green = []
 
     # Convert BGR to HSV colorspace
     hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
 
     # Set range for green color
-    green_lower = np.array([40,50,50], np.uint8)
-    green_upper = np.array([85,255,255], np.uint8)
+    green_lower = np.array([45,60,60], np.uint8)
+    green_upper = np.array([80,245,245], np.uint8)
 
     # define mask
     green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
@@ -103,7 +76,7 @@ while True:
             x, y, w, h = cv2.boundingRect(contour)
 
             # Send x + w/2 and y + h/2 to path finding program
-            middle_green = list[x+w/2, y+h/2]
+            middle_green.append((x+w/2, y+h/2))
             imageFrame = cv2.circle(imageFrame, (int(x + w / 2), int(y + h / 2)),
                                     5,
                                     (0, 255, 0), 2)
@@ -126,7 +99,7 @@ while True:
             x, y, w, h = cv2.boundingRect(contour)
 
             # Send x + w/2 and y + h/2 to path finding program
-            middle_blue = list[x+w/2, y+h/2]
+            middle_blue.append((x+w/2, y+h/2))
 
             imageFrame = cv2.circle(imageFrame, (int(x + w / 2), int(y + h / 2)),
                                     5,
@@ -141,40 +114,15 @@ while True:
                         1.0, (255, 0, 0))
 
     print(middle_blue, middle_green)
+    if len(middle_green) != 0 and len(middle_blue) != 0:
+        for (x,y) in middle_green:
+            for (dx,dy) in middle_blue:
+                drawAxis(imageFrame, (x,y), (dx,dy), (0, 255, 0), 1)
+
+        # get angle from vector
+
     # Display the captured frame
     cv2.imshow("Color Detection", imageFrame)
-
-
-    # Adding directions
-
-    # Convert image to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Convert image to binary
-    _, bw = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-    # Find all the contours in the thresholded image
-    contours, _ = cv2.findContours(bw, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-
-    for i, c in enumerate(contours):
-
-        # Calculate the area of each contour
-        area = cv2.contourArea(c)
-
-        # Ignore contours that are too small or too large
-        if area < 3700 or 100000 < area:
-            continue
-
-        # Draw each contour only for visualisation purposes
-        cv2.drawContours(img, contours, i, (0, 0, 255), 2)
-
-        # Find the orientation of each shape
-        getOrientation(c, img)
-
-    cv2.imshow('Orientation Image', img)
-
-
-
 
     # Press 'q' to exit the loop
     if cv2.waitKey(1) == ord('q'):
