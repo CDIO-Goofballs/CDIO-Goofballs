f0b780b06e5af375c9a9d4044d2d50555fa55a1f
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2025-06-26 13:43:52 +0200

    Fix unit tests for point calculations

--- robot_controls/test_point.py
+++ robot_controls/test_point.py
@@ -1,39 +1,39 @@
 import unittest
-from pathfinding.point import calculate_distance, calculate_turn
+from pathfinding.point import calculate_distance, calculate_turn, MyPoint
 
 class TestPoint(unittest.TestCase):
 
     def test_distance_calculation(self):
-        p1 = (0, 0) 
-        p2 = (0, 200) 
+        p1 = MyPoint(0, 0)
+        p2 = MyPoint(0, 200)
         self.assertEqual(calculate_distance(p1, p2), 200)
 
     def test_zero_distance_calculation(self):
-        p1 = (0, 0) 
-        p2 = (0, 0)
+        p1 = MyPoint(0, 0)
+        p2 = MyPoint(0, 0)
         self.assertEqual(calculate_distance(p1, p2), 0)
 
     def test_angle_calculation(self):
-        p1 = (0, 0) 
-        p2 = (0, -1)
+        p1 = MyPoint(0, 0)
+        p2 = MyPoint(0, -1)
         orientation = 0
-        self.assertEqual(calculate_turn(p1, p2, orientation), 0)
+        self.assertEqual(calculate_turn(p1, p2, orientation), -90)
 
     def test_angle_antiparallel_calculation(self):
-        p1 = (0, 0) 
-        p2 = (-200, 0)
+        p1 = MyPoint(0, 0)
+        p2 = MyPoint(-200, 0)
         orientation = 0
         self.assertEqual(calculate_turn(p1, p2, orientation), -180)
 
 
     def test_turn_calculation_on_oblique_path(self):
         positions = [
-            (0,0),
-            (100,100),
-            (160,0),
-            (160,-120),
-            (40,100),
-            (0,0)
+            MyPoint(0,0),
+            MyPoint(100,100),
+            MyPoint(160,0),
+            MyPoint(160,-120),
+            MyPoint(40,100),
+            MyPoint(0,0)
         ]
         self.assertAlmostEqual(calculate_turn(positions[0], positions[1], 0), 45)
         self.assertAlmostEqual(calculate_turn(positions[1], positions[2], 45), -104.04, 1)
